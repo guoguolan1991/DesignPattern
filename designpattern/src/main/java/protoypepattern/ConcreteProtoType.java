dd14f281f8d592265a6c91a51282fa0c76658a80
guoguolan1991 <guoguolan1991@gmail.com>
2017-08-21 09:41:37 +0800

    add prototype pattern

---
+++ designpattern/src/main/java/protoypepattern/ConcreteProtoType.java
@@ -0,0 +1,10 @@
+package protoypepattern;
+
+/**
+ * Created by Miller on 2017/8/21 0021.
+ */
+public class ConcreteProtoType extends Prototype{
+    public void show(){
+        System.out.println("this is concrete prototype");
+    }
+}
